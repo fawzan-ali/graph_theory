c26f6c350aa0571db2a6620971ff0bafed0f2e47
fawzan-ali <fawzanali45@gmail.com>
2023-02-11 20:13:49 -0600

    implemented erdos function

--- includes/functions.hpp
+++ includes/functions.hpp
@@ -3,11 +3,11 @@
 #include <string>
 #include <vector>
 
-// returns a vector of non-negative integers sorted in non-increasing order 
-// from a given CSV file
-std::vector<int> degrees(std::string filename);
+// returns a vector of non-negative integers from a given CSV file
+std::vector<int> degrees(std::string &filename);
 // checks if given vector represents a graphic sequence using Havel-Hakimi algorithm
 bool isGraphicHavel(std::vector<int> d);
-// checks if given vector represents a graphic sequence using
-// the Erdős–Gallai theorem
-bool isGraphicErdos(std::vector<int> d);
+// checks if given vector represents a graphic sequence using the Erdős–Gallai theorem
+bool isGraphicErdos(std::vector<int> const &d);
+// returns the minimum of two unsigned ints (if equal, returns first)
+unsigned int min(unsigned int const &first, unsigned int const &second);
--- src/functions.cpp
+++ src/functions.cpp
@@ -2,8 +2,9 @@
 
 #include <fstream>
 #include <bits/stdc++.h>
+#include <cstdlib>
 
-std::vector<int> degrees(std::string filename) {
+std::vector<int> degrees(std::string &filename) {
     std::vector<int> degrees;
     std::ifstream file;
     file.open(filename);
@@ -12,10 +13,10 @@ std::vector<int> degrees(std::string filename) {
         degrees.push_back(std::stoi(number));
     }  
     file.close();
-    std::sort(degrees.rbegin(), degrees.rend());
     return degrees;
 }
 bool isGraphicHavel(std::vector<int> d) {
+    std::sort(d.rbegin(), d.rend());
     auto d_1 = d.at(0);
     auto d_n = d.at(d.size() - 1);
     if (d_n < 0) {
@@ -33,10 +34,40 @@ bool isGraphicHavel(std::vector<int> d) {
                 d_prime.push_back(d.at(i));
             }
         }
-        std::sort(d_prime.rbegin(), d_prime.rend());
         if (isGraphicHavel(d_prime)) {
             return true;
         }
+    }
+    return false;
+}
+unsigned int min(unsigned int const &first, unsigned int const &second) {
+    if (second < first) {
+        return second;
+    }
+    return first;
+}
+bool isGraphicErdos(std::vector<int> const &d) {
+    unsigned int sum = 0;
+    unsigned int n = d.size();
+    for (unsigned int i = 0; i < n; ++i) {
+        sum += abs(d.at(i));
+    }
+    for (unsigned int k = 0; k < n; ++k) {
+        unsigned LHS = 0;
+        unsigned RHS = 0;
+        for (unsigned int i = 0; i < k; ++i) {
+            LHS += abs(d.at(i));
+        }
+        for (unsigned int i = k; i < n; ++i) {
+            RHS += min(abs(d.at(i)), k);
+        }
+        RHS += k * (k + 1);
+        if (LHS > RHS) {
+            return false;
+        }
+    }
+    if ((sum % 2) == 0) {
+        return true;
     }
     return false;
 }
